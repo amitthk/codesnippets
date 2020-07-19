2e8b12b7db8baf6a252f7261f7a86569a82aaf41
amitthk <e.amitthakur@gmail.com>
2020-07-20 07:54:53 +0800

    fix typos

--- scala/starter/src/main/scala/orderSummary.scala
+++ scala/starter/src/main/scala/orderSummary.scala
@@ -1,9 +1,12 @@
 import scala.io.Source
 
-object OrderRevenue{
-    val orderItems = Source.fromFile("/Users/amitthk/projects/data-master/retail_db/order_items/part-00000").getLines
-    val orderRevenue = orderItems.filter( orderitem => orderitem.split(",")(1).toInt == 2).
-    map(orderitem => orderitem.split(",")(4).toFloat).
-    reduce((t,v) => t+v)
-    println(OrderRevenue)
+object orderSummary{
+    def main(args: Array[String]) = {
+        val orderId = args(1).toInt
+        val orderItems = Source.fromFile("/Users/amitthk/projects/data-master/retail_db/order_items/part-00000").getLines
+        val orderRevenue = orderItems.filter(orderitem => orderitem.split(",")(1).toInt == orderId).
+          map(orderitem => orderitem.split(",")(4).toFloat).
+          reduce((t, v) => t + v)
+        println(orderRevenue)
+    }
 }
