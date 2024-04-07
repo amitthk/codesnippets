ee9997f894205de8439c82f08d189f8e8683000c
amitthk <e.amitthakur@gmail.com>
2024-04-07 12:23:21 +0800

    github actions

--- jupyter-notebooks/notebook_config.py
+++ jupyter-notebooks/notebook_config.py
@@ -1,6 +1,6 @@
 c = get_config()
-c.NotebookApp.certfile = u'/usr/share/ssl/stridecal.crt'
-c.NotebookApp.keyfile = u'/usr/share/ssl/stridecal.key'
+c.NotebookApp.certfile = u'/usr/share/ssl/stical.crt'
+c.NotebookApp.keyfile = u'/usr/share/ssl/stical.key'
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.open_browser = False
 c.NotebookApp.password = '<hash>'
